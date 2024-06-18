500929c404681ed6ac27e3298468a43d8a802660
maitedlapierre <dienefer.2022311842@aluno.iffar.edu.br>
2024-06-18 17:11:07 -0300

    Update conexao.php
    atualização

--- conexao.php
+++ conexao.php
@@ -5,12 +5,23 @@
  * @return \mysqli retorna uma conexão com à base de dados, ou em caso de falha, mata a execução e exibe o erro. 
  */
 function conectar() {
-    $conexao= mysqli_connect("localhost", "root", "", "recuperar-senha"); 
+$conexao= mysqli_connect("localhost", "root", "", "recuperar-senha"); 
     if($conexao === false) {
         echo "Erro ao conectar à base de dados. Nº do erro: " . mysqli_connect_errno() . "." . mysqli_connect_errno();
         die();
     }
+
     return $conexao; 
+}
+
+function executarSQL($conexao, $sql) {
+$resultado= mysqli_query($conexao, $sql);
+    if($resultado === false) {
+        echo "Erro ao executar comando SQL." . mysqli_errno($conexao) . " : " . mysqli_error($conexao);
+        die();
+    }
+
+    return $resultado;
 }
 
 ?>
